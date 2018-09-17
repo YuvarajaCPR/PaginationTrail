8e2df5a5e129f6d47f96bfc29d3f18a38afea986
kathir <kathir.ellora@gmail.com>
2018-09-17 15:51:11 +0530

    commit two

--- MovieVH.java
+++ MovieVH.java
@@ -14,6 +14,7 @@ class MovieVH extends RecyclerView.ViewHolder {
     public MovieVH(View itemView) {
         super(itemView);
         //comment line
+        //push to github
         textView = (TextView) itemView.findViewById(R.id.value);
     }
 }
