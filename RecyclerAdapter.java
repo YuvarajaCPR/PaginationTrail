bd7bf94408837150f008dc600952c1a47ebea31f
ULAGAPANDI UPMS <42641574+ulagapandi2upms@users.noreply.github.com>
2018-09-19 12:22:11 +0530

    Master is Changed by ulaga

--- RecyclerAdapter.java
+++ RecyclerAdapter.java
@@ -18,7 +18,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
 
     public LayoutInflater inflater;
     List<String> data = new ArrayList<>();
-    //Master is Checked at 11pm
+    //Master is Checked at 11pm changed by ulaga
     public RecyclerAdapter(List<String> data) {
         this.data = data;
     }
