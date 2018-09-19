3e699ef4ca1767ac6666394dacd2a707b1c28f4a
Yuvaraja <yuvarajacpyr17@gmail.com>
2018-09-19 16:23:35 +0530

    Line2 is Added

--- RecyclerAdapter.java
+++ RecyclerAdapter.java
@@ -1,8 +1,1 @@
-//Yuva changed this file Commit 1
-//Yuva changed this file Commit 2
-//Check Commit File 1
-//Check Commit File 2
-//Check Commit File 3
-//Line one added by YUvaraja
-//Line 2 is Added by Yuva
-//Line 4 is Added by Yuva
+//Line 2
